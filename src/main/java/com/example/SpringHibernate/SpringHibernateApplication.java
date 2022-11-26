990212c132cdee93a64bb32b1a9a6ffb5c6354d6
shev-81 <shev-81@mail.ru>
2022-11-26 09:54:46 +0300

    ignores

---
+++ src/main/java/com/example/SpringHibernate/SpringHibernateApplication.java
@@ -0,0 +1,17 @@
+package com.example.SpringHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Тестовый проект для работы с Hibernate!
+ */
+@SpringBootApplication
+public class SpringHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateApplication.class, args);
+	}
+
+}
